631e8af972670008a9dab544db605c7d24a1e351
black-trooper <hermanmiller.dkr@gmail.com>
2017-09-25 10:04:51 +0900

    Update docs

--- docs/build.js
+++ docs/build.js
@@ -49,7 +49,7 @@ riot.tag2("app", '<navigation></navigation> <div style="margin-left:15.5rem"> <d
 },{"../package.json":2}],2:[function(require,module,exports){
 module.exports={
   "name": "semantic-ui-riot",
-  "version": "0.0.10",
+  "version": "0.0.11",
   "description": "Semantic UI for Riot",
   "main": "dist/semantic-ui-riot.min.js",
   "files": [
