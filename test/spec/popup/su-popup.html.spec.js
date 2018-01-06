1669527689135d626c60204b411a9afcbcd933e0
black-trooper <hermanmiller.dkr@gmail.com>
2018-01-06 22:49:22 +0900

    Add test for su-popup

---
+++ test/spec/popup/su-popup.html.spec.js
@@ -0,0 +1,22 @@
+describe('su-popup.html', function () {
+  let tag
+  const spyOnMouseover = sinon.spy()
+  const spyOnMouseout = sinon.spy()
+
+  beforeEach(function () {
+    $('body').append('<su-popup><i class="add icon"></i><su-popup-content><div class="header">User Rating</div></su-popup-content></su-popup>')
+    tag = riot.mount('su-popup')[0]
+    tag.on('mouseover', spyOnMouseover)
+    tag.on('mouseout', spyOnMouseout)
+  })
+
+  afterEach(function () {
+    spyOnMouseover.reset()
+    spyOnMouseout.reset()
+    tag.unmount()
+  })
+
+  it('html content', function () {
+    tag.content.should.equal('<div class="header">User Rating</div>')
+  })
+})
