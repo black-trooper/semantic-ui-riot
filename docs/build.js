15a3cb69370e5afcf1c23f7e51f4636288c87190
black-trooper <hermanmiller.dkr@gmail.com>
2018-01-23 10:32:26 +0900

    Update docs

--- docs/build.js
+++ docs/build.js
@@ -478,7 +478,7 @@ riot.tag2('navigation', '<div class="ui inverted vertical left fixed menu"> <div
 },{"../package.json":2}],2:[function(require,module,exports){
 module.exports={
   "name": "semantic-ui-riot",
-  "version": "0.3.2",
+  "version": "0.4.0",
   "description": "Semantic UI for Riot",
   "main": "dist/semantic-ui-riot.min.js",
   "files": [
