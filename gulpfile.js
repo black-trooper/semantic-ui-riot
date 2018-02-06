cf4b4fd15c0641acb3c94252f87c5b59bcd5f942
black-trooper <hermanmiller.dkr@gmail.com>
2018-02-07 08:32:21 +0900

    Refactor gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -26,18 +26,37 @@ const demo_watch_target = [
   'docs/tags/**/*.tag'
 ]
 
-// TASK
-// ============================================
+// ===================================================================================
+//                                                                             Default
+//                                                                             =======
 gulp.task('default', function () {
   return sequence(
     'webserver',
-    'htmlhint',
-    'eslint',
-    'clean',
-    'build',
-    'demo_build',
     'watch',
     'demo_watch'
+  );
+});
+
+// ===================================================================================
+//                                                                               Build
+//                                                                               =====
+gulp.task('watch', function () {
+  gulp.watch(watch_target, function () {
+    sequence(
+      'build'
+    );
+  });
+});
+
+gulp.task('build', function () {
+  return sequence(
+    'clean',
+    'htmlhint',
+    'eslint',
+    'compile',
+    'concat',
+    'compress',
+    'demo_copy'
   );
 });
 
@@ -47,13 +66,18 @@ gulp.task('clean', function () {
   ]);
 });
 
-gulp.task('build', function () {
-  return sequence(
-    'compile',
-    'concat',
-    'compress',
-    'demo_copy'
-  );
+gulp.task('htmlhint', function () {
+  return gulp.src(['**/*.tag', '!node_modules/**'])
+    .pipe(plumber())
+    .pipe(htmlhint('.htmlhintrc'))
+    .pipe(htmlhint.failReporter())
+});
+
+gulp.task('eslint', function () {
+  return gulp.src(['**/*.tag', 'test/spec/**/*.js', '!node_modules/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
 gulp.task('compile', function () {
@@ -79,32 +103,9 @@ gulp.task('compress', function (cb) {
     .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('watch', function () {
-  gulp.watch(watch_target, function () {
-    sequence(
-      'htmlhint',
-      'eslint',
-      'clean',
-      'build'
-    );
-  });
-});
-
-gulp.task('eslint', function () {
-  return gulp.src(['**/*.tag', 'test/spec/**/*.js', '!node_modules/**'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
-
-gulp.task('htmlhint', function () {
-  return gulp.src(['**/*.tag', '!node_modules/**'])
-    .pipe(plumber())
-    .pipe(htmlhint('.htmlhintrc'))
-    .pipe(htmlhint.failReporter())
-})
-
-// for demo
+// ===================================================================================
+//                                                                          Demo Build
+//                                                                          ==========
 gulp.task('demo_watch', function () {
   gulp.watch(demo_watch_target, function () {
     sequence(
@@ -115,6 +116,7 @@ gulp.task('demo_watch', function () {
 
 gulp.task('demo_build', function () {
   return sequence(
+    'demo_clean',
     'demo_escape',
     'demo_htmlhint',
     'demo_compile',
@@ -123,6 +125,14 @@ gulp.task('demo_build', function () {
     'demo_compress',
     'demo_clean'
   );
+});
+
+
+gulp.task('demo_clean', function (cb) {
+  return del([
+    '**/temp.js'
+    , 'docs/temp'
+  ], cb);
 });
 
 gulp.task('demo_escape', function () {
@@ -170,18 +180,14 @@ gulp.task('demo_compress', function (cb) {
     .pipe(gulp.dest('docs/'));
 });
 
-gulp.task('demo_clean', function (cb) {
-  return del([
-    '**/temp.js'
-    , 'docs/temp'
-  ], cb);
-});
-
 gulp.task('demo_copy', function () {
   return gulp.src('dist/semantic-ui-riot.min.js')
     .pipe(gulp.dest('docs/'))
 })
 
+// ===================================================================================
+//                                                                          Web Server
+//                                                                          ==========
 gulp.task('webserver', function () {
   const target = [
     'index.html',
