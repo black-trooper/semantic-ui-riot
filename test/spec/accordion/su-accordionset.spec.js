1d2eba6a72d29d4cdb1b3e5317587c0d548caa7f
black-trooper <hermanmiller.dkr@gmail.com>
2018-04-29 20:48:16 +0900

    Add test for su-accordion

---
+++ test/spec/accordion/su-accordionset.spec.js
@@ -0,0 +1,39 @@
+const fireEvent = require('../../helpers').fireEvent
+
+describe('su-accordionset', function () {
+  let tag
+  let spyOnClick = sinon.spy()
+
+  beforeEach(function () {
+    const group = $('<su-accordionset></su-accordionset>')
+    group.append('<su-accordion title="Home">Home content</su-accordion>')
+      .append('<su-accordion title="Message">Messages content</su-accordion>')
+    $('body').append(group)
+    tag = riot.mount('su-accordionset')[0]
+    tag.on('click', spyOnClick)
+  })
+
+  afterEach(function () {
+    spyOnClick.reset()
+    tag.unmount()
+  })
+
+  it('is mounted', function () {
+    tag.isMounted.should.be.true
+  })
+
+  it('click title', function () {
+    tag.tags['su-accordion'][0].active.should.equal(true)
+    tag.tags['su-accordion'][1].active.should.equal(false)
+
+    fireEvent($('div.title:eq(1)')[0], 'click')
+    tag.tags['su-accordion'][0].active.should.equal(false)
+    tag.tags['su-accordion'][1].active.should.equal(true)
+    spyOnClick.should.have.been.calledOnce
+
+    fireEvent($('div.title:eq(0)')[0], 'click')
+    tag.tags['su-accordion'][0].active.should.equal(true)
+    tag.tags['su-accordion'][1].active.should.equal(false)
+    spyOnClick.should.have.been.calledTwice
+  })
+})
