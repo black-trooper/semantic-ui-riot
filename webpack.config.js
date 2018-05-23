aa26ef78663fce50ebc8a6c9d28962d90c3c18c9
black-trooper <hermanmiller.dkr@gmail.com>
2018-05-23 09:43:03 +0900

    Use webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,43 @@
 const webpack = require('webpack');
 const path = require('path');
 
-module.exports = {
-  output: {
-    path: __dirname + '/dist/',
-    filename: 'semantic-ui-riot.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tag$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'riotjs-loader',
-            options: {
-              type: 'es6', // transpile the riot tags using babel
-              hot: true
-            }
-          },
-          {
-            loader: 'eslint-loader',
-            options: {
-              fix: true,
-              emitWarning: true,
-            }
-          },
-        ]
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      }
-    ]
-  },
-  devtool: false,
-  // plugins: [
-  //   new webpack.LoaderOptionsPlugin({ options: {} }),
-  //   new webpack.ProvidePlugin({
-  //     riot: 'riot',
-  //   })
-  // ]
+module.exports = (env, argv) => {
+  return {
+    output: {
+      path: __dirname + '/dist/',
+      filename: argv.mode === 'production' ? 'semantic-ui-riot.min.js' : 'semantic-ui-riot.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tag$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'riotjs-loader',
+              options: {
+                type: 'es6', // transpile the riot tags using babel
+                hot: true
+              }
+            },
+            {
+              loader: 'eslint-loader',
+              options: {
+                fix: true,
+                emitWarning: true,
+              }
+            },
+            // TODO HTML hint-loader
+          ]
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        }
+        // TODO .spec.js eslint-loeader
+      ]
+    },
+    devtool: false,
+  }
 }
