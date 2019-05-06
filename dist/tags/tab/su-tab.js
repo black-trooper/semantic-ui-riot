0bdd958fcaeeb516985b85c6283158f10237c28b
black-trooper <hermanmiller.dkr@gmail.com>
2019-05-06 23:08:07 +0900

    Support v4 in su-tab

--- dist/tags/tab/su-tab.js
+++ dist/tags/tab/su-tab.js
@@ -1,18 +1,57 @@
-riot.tag2('su-tab', '<virtual if="{mounted}"><yield></yield></virtual>', 'su-tab.ui.segment,[data-is="su-tab"].ui.segment{ margin-top: 0; margin-bottom: 0; } su-tab.ui.segment.top.attached,[data-is="su-tab"].ui.segment.top.attached{ margin-top: 0 } su-tab.ui.segment.bottom.attached,[data-is="su-tab"].ui.segment.bottom.attached{ margin-bottom: 0 }', 'class="ui {opts.class} {active: active} tab"', function(opts) {
-    const tag = this
+function onMounted(props, state) {
+  this.update();
+}
 
-    tag.active = false
-    tag.mounted = false
+function onUpdated(props, state) {
+  if (state.active && !state.mounted) {
+    state.mounted = true;
+  }
+}
 
-    tag.on('mount', onMount)
-    tag.on('update', onUpdate)
+var suTab = {
+  'css': `su-tab.ui.segment,[is="su-tab"].ui.segment{
+      margin-top: 0;
+      margin-bottom: 0;
+    } su-tab.ui.segment.top.attached,[is="su-tab"].ui.segment.top.attached{
+      margin-top: 0
+    } su-tab.ui.segment.bottom.attached,[is="su-tab"].ui.segment.bottom.attached{
+      margin-bottom: 0
+    }`,
 
-    function onMount() {
-      tag.update()
-    }
-    function onUpdate() {
-      if (tag.active && !tag.mounted) {
-        tag.mounted = true
-      }
-    }
-});
+  'exports': {
+    state: {
+      active: false,
+      mounted: false,
+    },
+
+    onMounted,
+    onUpdated
+  },
+
+  'template': function(template, expressionTypes, bindingTypes, getComponent) {
+    return template('<p expr32></p>', [{
+      'expressions': [{
+        'type': expressionTypes.ATTRIBUTE,
+        'name': 'class',
+
+        'evaluate': function(scope) {
+          return ['ui ', scope.props.class, ' ', scope.state.active && 'active'].join('');
+        }
+      }]
+    }, {
+      'type': bindingTypes.IF,
+
+      'evaluate': function(scope) {
+        return scope.state.mounted;
+      },
+
+      'redundantAttribute': 'expr32',
+      'selector': '[expr32]',
+      'template': template('<slot></slot>', [])
+    }]);
+  },
+
+  'name': 'su-tab'
+};
+
+export default suTab;
