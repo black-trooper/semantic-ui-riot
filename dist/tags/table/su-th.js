4eac46972637d1915ebea90cbae9e71576ee7465
black-trooper <hermanmiller.dkr@gmail.com>
2019-05-20 23:54:07 +0900

    Support v4 in su-th

--- dist/tags/table/su-th.js
+++ dist/tags/table/su-th.js
@@ -1,9 +1,32 @@
-riot.tag2('su-th', '', '', 'onclick="{click}" class="{sorted: sorted} {ascending: sorted && !reverse} {descending: sorted && reverse}"', function(opts) {
-    const tag = this
+function onBeforeUpdate(props, state) {
+  const temp = [];
+  if (state.sorted) {
+    temp.push('sorted ');
+    temp.push(stat.reverse ? 'descending' : 'ascending');
+  }
+  this.classes = temp.join(' ');
+}
 
-    tag.click = click
+function onClick() {
+  this.obs.trigger(`${su_id}-click`, opts.field);
+}
 
-    function click() {
-      tag.trigger('click', opts.field)
-    }
-});
+var suTh = {
+  'css': null,
+
+  'exports': {
+    state: {
+      sorted: false,
+      reverse: false,
+    },
+
+    classes: '',
+    onBeforeUpdate,
+    onClick
+  },
+
+  'template': null,
+  'name': 'su-th'
+};
+
+export default suTh;
