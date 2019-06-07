8317108ace0993106f145298fba4d76276f6ac3b
black-trooper <hermanmiller.dkr@gmail.com>
2019-06-07 23:19:14 +0900

    Support v4 in su-rating

--- dist/tags/rating/su-rating.js
+++ dist/tags/rating/su-rating.js
@@ -1,92 +1,186 @@
-riot.tag2('su-rating', '<i class="icon {active: item.active} {selected: item.selected}" each="{item in items}" onclick="{click.bind(this, item)}" onmouseover="{mouseover.bind(this, item)}" onmouseout="{mouseout}"></i>', '', 'class="ui rating {opts.class}"', function(opts) {
-    const tag = this
+// ===================================================================================
+//                                                                           Lifecycle
+//                                                                           =========
+function onBeforeMount(props, state) {
+  init(this, props.max, props.value);
+  this.obs.on(`${this.su_id}-reset`, () => { reset(this); });
+}
 
-    tag.items = []
+function onBeforeUpdate(props, state) {
+  this.readOnly = this.root.classList.contains('read-only');
+  this.changed = state.value != state.defaultValue;
+  updateView(this);
+}
 
-    tag.reset = reset
-    tag.changed = changed
-    tag.click = click
-    tag.mouseout = mouseout
-    tag.mouseover = mouseover
-    tag.on('mount', onMount)
-    tag.on('update', onUpdate)
+// ===================================================================================
+//                                                                              Events
+//                                                                              ======
+function onClick(target) {
+  if (this.readOnly) {
+    return
+  }
+  let valueChanged = false;
+  let beforeValue;
+  if (this.state.value != target.value) {
+    beforeValue = this.state.value;
+    valueChanged = true;
+  }
+  this.state.value = target.value;
+  updateView(this);
+  this.update();
 
-    function onMount() {
-      init(opts.max, opts.value)
-    }
+  this.dispatch('click', target.value);
+  if (valueChanged) {
+    this.dispatch('change', { value: this.state.value, beforeValue: beforeValue });
+  }
+}
 
-    function onUpdate() {
-      updateView()
-    }
+function onMouseover(target) {
+  if (this.readOnly) {
+    return
+  }
+  this.state.items.forEach(item => {
+    item.selected = item.value <= target.value;
+  });
+  this.update();
+}
 
-    function reset() {
-      tag.value = tag.defaultValue
-    }
+function onMouseout() {
+  if (this.readOnly) {
+    return
+  }
+  this.state.items.forEach(item => {
+    item.selected = false;
+  });
+  this.update();
+}
 
-    function changed() {
-      return tag.value != tag.defaultValue
-    }
+// ===================================================================================
+//                                                                               Logic
+//                                                                               =====
+function init(tag, max = 5, value = 0) {
+  tag.state.value = value;
+  tag.state.defaultValue = value;
+  tag.state.items.length = 0;
+  for (let i = 0; i < max; i++) {
+    tag.state.items[i] = { value: i + 1, active: false, selected: false };
+  }
+  updateView(tag);
+}
 
-    function click(target) {
-      if (isReadOnly()) {
-        return
+function reset(tag) {
+  tag.update({
+    value: tag.state.defaultValue
+  });
+}
+
+function updateView(tag) {
+  tag.state.items.forEach(item => {
+    item.active = item.value <= tag.state.value;
+  });
+}
+
+var suRating = {
+  'css': null,
+
+  'exports': {
+    state: {
+      items: [],
+      defaultValue: null,
+    },
+
+    changed: false,
+    onBeforeMount,
+    onBeforeUpdate,
+    onClick,
+    onMouseover,
+    onMouseout
+  },
+
+  'template': function(template, expressionTypes, bindingTypes, getComponent) {
+    return template('<i expr97></i>', [{
+      'expressions': [{
+        'type': expressionTypes.ATTRIBUTE,
+        'name': 'class',
+
+        'evaluate': function(scope) {
+          return ['ui rating ', scope.props.class].join('');
+        }
+      }, {
+        'type': expressionTypes.VALUE,
+
+        'evaluate': function(scope) {
+          return scope.state.value;
+        }
+      }, {
+        'type': expressionTypes.ATTRIBUTE,
+        'name': 'changed',
+
+        'evaluate': function(scope) {
+          return scope.changed;
+        }
+      }, {
+        'type': expressionTypes.ATTRIBUTE,
+        'name': 'id',
+
+        'evaluate': function(scope) {
+          return scope.su_id;
+        }
+      }]
+    }, {
+      'type': bindingTypes.EACH,
+      'getKey': null,
+      'condition': null,
+
+      'template': template(null, [{
+        'expressions': [{
+          'type': expressionTypes.ATTRIBUTE,
+          'name': 'class',
+
+          'evaluate': function(scope) {
+            return [
+              'icon ',
+              scope.item.active ? 'active' : '',
+              ' ',
+              scope.item.selected ? 'selected' : ''
+            ].join('');
+          }
+        }, {
+          'type': expressionTypes.EVENT,
+          'name': 'onclick',
+
+          'evaluate': function(scope) {
+            return () => scope.onClick(scope.item);
+          }
+        }, {
+          'type': expressionTypes.EVENT,
+          'name': 'onmouseover',
+
+          'evaluate': function(scope) {
+            return () => scope.onMouseover(scope.item);
+          }
+        }, {
+          'type': expressionTypes.EVENT,
+          'name': 'onmouseout',
+
+          'evaluate': function(scope) {
+            return scope.onMouseout;
+          }
+        }]
+      }]),
+
+      'redundantAttribute': 'expr97',
+      'selector': '[expr97]',
+      'itemName': 'item',
+      'indexName': null,
+
+      'evaluate': function(scope) {
+        return scope.state.items;
       }
-      let valueChanged = false
-      let beforeValue
-      if (tag.value != target.value) {
-        beforeValue = tag.value
-        valueChanged = true
-      }
-      tag.value = target.value
-      updateView()
-      parentUpdate()
-      tag.trigger('click', target.value)
-      if (valueChanged) {
-        tag.trigger('change', { value: tag.value, beforeValue: beforeValue })
-      }
-    }
+    }]);
+  },
 
-    function mouseover(target) {
-      if (isReadOnly()) {
-        return
-      }
-      tag.items.forEach(item => {
-        item.selected = item.value <= target.value
-      })
-    }
+  'name': 'su-rating'
+};
 
-    function mouseout() {
-      tag.items.forEach(item => {
-        item.selected = false
-      })
-    }
-
-    function isReadOnly() {
-      return tag.root.classList.contains('read-only')
-    }
-
-    function init(max = 5, value = 0) {
-      tag.value = value
-      tag.defaultValue = value
-      tag.items.length = 0
-      for (let i = 0; i < max; i++) {
-        tag.items[i] = { value: i + 1, active: false, selected: false }
-      }
-      updateView()
-      parentUpdate()
-    }
-
-    function updateView() {
-      tag.items.forEach(item => {
-        item.active = item.value <= tag.value
-      })
-    }
-
-    function parentUpdate() {
-      if (tag.parent) {
-        tag.parent.update()
-      } else {
-        tag.update()
-      }
-    }
-});
+export default suRating;
