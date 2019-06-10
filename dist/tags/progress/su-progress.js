8e2e9792dd0c97912f47bd44cd1d3389dd4f6dc6
black-trooper <hermanmiller.dkr@gmail.com>
2019-06-10 20:23:32 +0900

    Support v4 in su-progress

--- dist/tags/progress/su-progress.js
+++ dist/tags/progress/su-progress.js
@@ -1,97 +1,173 @@
-riot.tag2('su-progress', '<div class="ui progress {getClass()} {getStates()}" data-percent="{percent}"> <div class="bar" riot-style="transition-duration: 300ms; width: {percent}%;"> <div if="{isProgress()}" class="progress">{percent}%</div> </div> <div class="label"> <yield></yield> </div> </div>', 'su-progress .ui.progress:last-child,[data-is="su-progress"] .ui.progress:last-child{ margin: 0 0 2.5em; } su-progress.attached,[data-is="su-progress"].attached{ display: block; height: 0.2rem; padding: 0px; overflow: hidden; border-radius: 0em 0em 0.28571429rem 0.28571429rem; position: absolute; left: 0; width: 100%; } su-progress.top.attached,[data-is="su-progress"].top.attached{ top: 0px; bottom: 100%; border-radius: 0.28571429rem 0.28571429rem 0em 0em; } su-progress.bottom.attached,[data-is="su-progress"].bottom.attached{ top: 100%; bottom: auto; }', 'class="{opts.class}"', function(opts) {
-    const tag = this
+// ===================================================================================
+//                                                                           Lifecycle
+//                                                                           =========
+function onMounted() {
+  this.value = props.value || 0;
+  if (props.total > 0) {
+    total = props.total;
+  }
+  this.percent = getPercent(this);
+  this.lastValue = this.value;
+  this.lastPropsValue = props.value;
+  this.update();
+}
 
-    tag.defaultValue = null
-    tag.value = null
+function onUpdated() {
+  let changed = false;
+  if (this.state.value >= total) {
+    this.state.value = total;
+  }
+  if (this.state.value <= 0) {
+    this.state.value = 0;
+  }
+  if (this.lastValue != this.state.value) {
+    this.lastValue = this.state.value;
+    changed = true;
+  } else if (this.lastPropsValue != props.value) {
+    this.state.value = props.value;
+    this.lastPropsValue = props.value;
+    this.lastValue = props.value;
+    changed = true;
+  }
 
-    tag.getClass = getClass
-    tag.getStates = getStates
-    tag.isProgress = isProgress
-    tag.on('mount', onMount)
-    tag.on('update', onUpdate)
+  if (changed) {
+    this.percent = getPercent(this);
+  }
+}
 
-    let lastOptsValue = null
-    let lastValue = null
-    let total = 100
+// ===================================================================================
+//                                                                              Helper
+//                                                                              ======
+function getClass() {
+  const excludeClasses = ['progress', 'active'];
+  return Array.apply(null, this.root.classList).filter(clazz => {
+    return !excludeClasses.some(excludeClass => excludeClass == clazz)
+  }).join(' ')
+}
 
-    function onMount() {
-      if (typeof opts.riotValue === 'undefined' && typeof opts.value !== 'undefined') {
-        opts.riotValue = opts.value
-      }
-      init(opts.riotValue, opts.total)
+function getStates() {
+  if (isSuccess(this)) {
+    return 'success'
+  }
+  if (isActive(this)) {
+    return 'active'
+  }
+}
 
-      tag.update()
-      tag.defaultValue = tag.value
-    }
+function isProgress() {
+  return hasClass(this, 'progress')
+}
 
-    function onUpdate() {
-      let changed = false
-      if (tag.value >= total) {
-        tag.value = total
-      }
-      if (tag.value <= 0) {
-        tag.value = 0
-      }
-      if (lastValue != tag.value) {
-        lastValue = tag.value
-        changed = true
-      } else if (lastOptsValue != opts.riotValue) {
-        tag.value = opts.riotValue
-        lastOptsValue = opts.riotValue
-        lastValue = opts.riotValue
-        changed = true
-      }
+// ===================================================================================
+//                                                                               Logic
+//                                                                               =====
+function getPercent(tag) {
+  return parseInt(tag.value / total * 100)
+}
 
-      if (changed) {
-        tag.percent = getPercent()
-      }
-    }
+function isActive(tag) {
+  return hasClass(tag, 'active') && tag.percent > 0 && tag.percent < 100
+}
 
-    function getClass() {
-      const excludeClasses = ['progress', 'active']
-      return Array.apply(null, tag.root.classList).filter(clazz => {
-        return !excludeClasses.some(excludeClass => excludeClass == clazz)
-      }).join(' ')
-    }
+function isSuccess(tag) {
+  return tag.percent == 100
+}
 
-    function getStates() {
-      if (isSuccess()) {
-        return 'success'
-      }
-      if (isActive()) {
-        return 'active'
-      }
-    }
+function hasClass(tag, className) {
+  return tag.root.classList.contains(className)
+}
 
-    function isProgress() {
-      return hasClass('progress')
-    }
+var suProgress = {
+  'css': `su-progress .ui.progress:last-child,[is="su-progress"] .ui.progress:last-child{ margin: 0 0 2.5em; } su-progress.attached,[is="su-progress"].attached{ display: block; height: 0.2rem; padding: 0px; overflow: hidden; border-radius: 0em 0em 0.28571429rem 0.28571429rem; position: absolute; left: 0; width: 100%; } su-progress.top.attached,[is="su-progress"].top.attached{ top: 0px; bottom: 100%; border-radius: 0.28571429rem 0.28571429rem 0em 0em; } su-progress.bottom.attached,[is="su-progress"].bottom.attached{ top: 100%; bottom: auto; }`,
 
-    function init(optsValue, optsTotal) {
-      if (tag.value == null) {
-        tag.value = optsValue || 0
-      }
-      if (optsTotal > 0) {
-        total = optsTotal
-      }
-      tag.percent = getPercent()
-      lastValue = tag.value
-      lastOptsValue = optsValue
-    }
+  'exports': {
+    state: {
+      value: null,
+    },
 
-    function getPercent() {
-      return parseInt(tag.value / total * 100)
-    }
+    lastPropsValue: null,
+    lastValue: null,
+    total: 100,
+    onMounted,
+    onUpdated,
+    getClass,
+    getStates,
+    isProgress
+  },
 
-    function isActive() {
-      return hasClass('active') && tag.percent > 0 && tag.percent < 100
-    }
+  'template': function(template, expressionTypes, bindingTypes, getComponent) {
+    return template(
+      '<div expr62><div expr63 class="bar"><div expr64 class="progress"></div></div><div class="label"><slot expr65></slot></div></div>',
+      [{
+        'expressions': [{
+          'type': expressionTypes.ATTRIBUTE,
+          'name': 'class',
 
-    function isSuccess() {
-      return tag.percent == 100
-    }
+          'evaluate': function(scope) {
+            return scope.props.class;
+          }
+        }]
+      }, {
+        'redundantAttribute': 'expr62',
+        'selector': '[expr62]',
 
-    function hasClass(className) {
-      return tag.root.classList.contains(className)
-    }
-});
+        'expressions': [{
+          'type': expressionTypes.ATTRIBUTE,
+          'name': 'class',
+
+          'evaluate': function(scope) {
+            return ['ui progress ', scope.getClass(), ' ', scope.getStates()].join('');
+          }
+        }, {
+          'type': expressionTypes.ATTRIBUTE,
+          'name': 'data-percent',
+
+          'evaluate': function(scope) {
+            return scope.percent;
+          }
+        }]
+      }, {
+        'redundantAttribute': 'expr63',
+        'selector': '[expr63]',
+
+        'expressions': [{
+          'type': expressionTypes.ATTRIBUTE,
+          'name': 'style',
+
+          'evaluate': function(scope) {
+            return ['transition-duration: 300ms; width: ', scope.percent, '%;'].join('');
+          }
+        }]
+      }, {
+        'type': bindingTypes.IF,
+
+        'evaluate': function(scope) {
+          return scope.isProgress();
+        },
+
+        'redundantAttribute': 'expr64',
+        'selector': '[expr64]',
+
+        'template': template('<!---->', [{
+          'expressions': [{
+            'type': expressionTypes.TEXT,
+            'childNodeIndex': 0,
+
+            'evaluate': function(scope) {
+              return [scope.percent, '%'].join('');
+            }
+          }]
+        }])
+      }, {
+        'type': bindingTypes.SLOT,
+        'name': 'default',
+        'redundantAttribute': 'expr65',
+        'selector': '[expr65]'
+      }]
+    );
+  },
+
+  'name': 'su-progress'
+};
+
+export default suProgress;
