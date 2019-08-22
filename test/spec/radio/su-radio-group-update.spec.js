21f09516727d21c8b481b3409918019e0c8be896
black-trooper <hermanmiller.dkr@gmail.com>
2019-08-22 21:15:20 +0900

    Fix problem when su-radio is missing in su-radio-group

---
+++ test/spec/radio/su-radio-group-update.spec.js
@@ -0,0 +1,84 @@
+import * as riot from 'riot'
+import { init, fireEvent, compile } from '../../helpers/'
+import RadioGroupComponent from '../../../dist/tags/radio/su-radio-group.js'
+import RadioComponent from '../../../dist/tags/radio/su-radio.js'
+
+describe('su-radio-group', function () {
+  let element, component
+  let spyOnChange = sinon.spy()
+  init(riot)
+
+  beforeEach(function () {
+    element = document.createElement('app')
+    riot.register('su-radio-group', RadioGroupComponent)
+    riot.register('su-radio', RadioComponent)
+    const AppComponent = compile(`
+      <app>
+        <su-radio-group
+          value="{ value }"
+          onchange="{ () => dispatch('change') }">
+          <su-radio each="{ item in items }" value="{ item }" label="Radio choice{ item }"></su-radio>
+        </su-radio-group>
+      </app>`)
+    riot.register('app', AppComponent)
+    component = riot.mount(element, {
+      'onchange': spyOnChange
+    })[0]
+  })
+
+  afterEach(function () {
+    spyOnChange.reset()
+    component.unmount()
+    riot.unregister('su-radio')
+    riot.unregister('su-radio-group')
+    riot.unregister('app')
+  })
+
+  it('is mounted', function () {
+    expect(component).to.be.ok
+  })
+
+  it('update value', function () {
+    expect(component.$$('su-radio').length).to.equal(0)
+    component.items = [1, 2]
+    component.update()
+
+    expect(component.$$('su-radio')[0].checked).to.be.not.ok
+    expect(component.$$('su-radio')[1].checked).to.be.not.ok
+
+    component.value = "1"
+    component.update()
+    expect(component.$$('su-radio')[0].checked).to.be.ok
+    expect(component.$$('su-radio')[1].checked).to.be.not.ok
+    spyOnChange.should.have.been.calledOnce
+
+    component.value = "2"
+    component.update()
+    expect(component.$$('su-radio')[0].checked).to.be.not.ok
+    expect(component.$$('su-radio')[1].checked).to.be.ok
+    spyOnChange.should.have.been.calledTwice
+
+    component.items = null
+    component.update()
+    expect(component.$$('su-radio').length).to.equal(0)
+  })
+
+  it('click radio', function () {
+    expect(component.$$('su-radio').length).to.equal(0)
+    component.items = [1, 2]
+    component.update()
+
+    expect(component.$$('su-radio')[0].checked).to.be.not.ok
+    expect(component.$$('su-radio')[1].checked).to.be.not.ok
+
+    component.$$('su-radio input')[0].click()
+    expect(component.$$('su-radio')[0].checked).to.be.ok
+    expect(component.$$('su-radio')[1].checked).to.be.not.ok
+    spyOnChange.should.have.been.calledOnce
+
+    component.$$('su-radio input')[1].click()
+    expect(component.$$('su-radio')[0].checked).to.be.not.ok
+    expect(component.$$('su-radio')[1].checked).to.be.ok
+    spyOnChange.should.have.been.calledTwice
+  })
+})
